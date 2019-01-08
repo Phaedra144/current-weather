ae8286630ea1aaf0cf62e85144550974b1aa3b8e
Phaedra144 <bedi.szilvia@gmail.com>
2019-01-08 22:50:37 +0100

    feat(mainactivity) adding edittext and getweather method

--- app/src/main/java/com/weather/szilvi/currentweather/MainActivity.java
+++ app/src/main/java/com/weather/szilvi/currentweather/MainActivity.java
@@ -2,17 +2,24 @@ package com.weather.szilvi.currentweather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText currentCity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        currentCity = findViewById(R.id.enterCityEditText);
     }
 
     public void getWeather(View view) {
-
+        String enteredCity = currentCity.getText().toString();
+        DownloadTask task = new DownloadTask();
+        task.execute("http://api.openweathermap.org/data/2.5/weather?q=" + enteredCity);
     }
 }
